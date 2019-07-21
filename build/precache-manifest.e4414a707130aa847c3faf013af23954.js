61fd3a81ef6b31e21fb75a39a43635d3697b9341
nnoradila <adila.nordin@gmail.com>
2019-07-22 01:11:23 +0800

    refresh for deploy

---
+++ build/precache-manifest.e4414a707130aa847c3faf013af23954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cebd778112eeb70adb62ed208a9a91",
+    "url": "/face-recognition-adila/index.html"
+  },
+  {
+    "revision": "99191f15c8bee908bd16",
+    "url": "/face-recognition-adila/static/css/main.729d8e0c.chunk.css"
+  },
+  {
+    "revision": "efb65ef88954723e48f0",
+    "url": "/face-recognition-adila/static/js/2.e96b978b.chunk.js"
+  },
+  {
+    "revision": "99191f15c8bee908bd16",
+    "url": "/face-recognition-adila/static/js/main.ca01e1bd.chunk.js"
+  },
+  {
+    "revision": "acdf8017c96f3f97657e",
+    "url": "/face-recognition-adila/static/js/runtime~main.f7c51332.js"
+  },
+  {
+    "revision": "ccd20247dfe4bef3cae08bbc4eeede84",
+    "url": "/face-recognition-adila/static/media/test.ccd20247.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/face-recognition-adila/precache-manifest.a7d23529d33abe09cd18da6d7ae9020d.js"
+  "/face-recognition-adila/precache-manifest.e4414a707130aa847c3faf013af23954.js"
 );
 
 self.addEventListener('message', (event) => {
