8d2e6bf9c4c4566efd6a0d0c4deaf83fd8d0bd77
nnoradila <adila.nordin@gmail.com>
2019-07-22 01:10:53 +0800

    Updates

--- precache-manifest.e4414a707130aa847c3faf013af23954.js
+++ precache-manifest.e4414a707130aa847c3faf013af23954.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a2af36258384b800372214f9fd455781",
+    "revision": "b0cebd778112eeb70adb62ed208a9a91",
     "url": "/face-recognition-adila/index.html"
   },
   {
-    "revision": "07708df6ab822e63dfb5",
+    "revision": "99191f15c8bee908bd16",
     "url": "/face-recognition-adila/static/css/main.729d8e0c.chunk.css"
   },
   {
-    "revision": "fc13b2e638ab66374c32",
-    "url": "/face-recognition-adila/static/js/2.a5aa172a.chunk.js"
+    "revision": "efb65ef88954723e48f0",
+    "url": "/face-recognition-adila/static/js/2.e96b978b.chunk.js"
   },
   {
-    "revision": "07708df6ab822e63dfb5",
-    "url": "/face-recognition-adila/static/js/main.3f52b32c.chunk.js"
+    "revision": "99191f15c8bee908bd16",
+    "url": "/face-recognition-adila/static/js/main.ca01e1bd.chunk.js"
   },
   {
     "revision": "acdf8017c96f3f97657e",
