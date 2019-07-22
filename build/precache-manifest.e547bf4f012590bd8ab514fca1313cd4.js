1b2d47531bf04814d1f3322f930a56c370f26878
nnoradila <adila.nordin@gmail.com>
2019-07-22 16:20:44 +0800

    recompiled

---
+++ build/precache-manifest.e547bf4f012590bd8ab514fca1313cd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f584d8138148ac5fff9189a240c2ef",
+    "url": "/face-recognition-adila/index.html"
+  },
+  {
+    "revision": "6148a8ea6217d2ce12e6",
+    "url": "/face-recognition-adila/static/css/main.729d8e0c.chunk.css"
+  },
+  {
+    "revision": "98363d81b79026df6a68",
+    "url": "/face-recognition-adila/static/js/2.9ae18c46.chunk.js"
+  },
+  {
+    "revision": "6148a8ea6217d2ce12e6",
+    "url": "/face-recognition-adila/static/js/main.79b4c02e.chunk.js"
+  },
+  {
+    "revision": "acdf8017c96f3f97657e",
+    "url": "/face-recognition-adila/static/js/runtime~main.f7c51332.js"
+  },
+  {
+    "revision": "ccd20247dfe4bef3cae08bbc4eeede84",
+    "url": "/face-recognition-adila/static/media/test.ccd20247.jpg"
+  }
+]);
